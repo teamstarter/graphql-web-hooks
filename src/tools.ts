7357c6868be3629fe6f1f2e15693b9967393a745
Vincent Desmares <vincent.desmares@gmail.com>
2022-12-25 10:14:48 +0100

    BREAKING CHANGE: Upgrade Apollo and all related libs.

--- src/tools.ts
+++ src/tools.ts
@@ -1,6 +1,14 @@
 import fetch from 'node-fetch'
 
-export async function postRequest({ url, data, headers }) {
+export async function postRequest({
+  url,
+  data,
+  headers,
+}: {
+  url: string
+  data: any
+  headers: any
+}) {
   await fetch(url, {
     method: 'post',
     body: JSON.stringify(data),
